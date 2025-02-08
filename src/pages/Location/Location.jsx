5cfb662170f112374f65eed767684c35def7fd97
Masianuy <Masianuy@ukr.net>
2025-02-08 23:52:20 +0200

    Services

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,11 +1,12 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
+import Services from '@/components/Services/Services';
 
 function Location() {
   return (
     <>
       <Header />
-      <div>Location</div>
+      <Services />
       <Footer />
     </>
   );
